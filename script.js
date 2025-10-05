1df79676eb5ad73467f8139dfaf7f83ae9281293
kgandashi-dotcom <kgandashi@gmail.com>
2025-10-05 19:24:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -328,6 +328,14 @@ function postToMake(payload){
   });
 }
 
+// פונקציה ליצירת UUID
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 function showMessage(txt, isError=true){
   const m = $id('messages');
   m.textContent = txt;
